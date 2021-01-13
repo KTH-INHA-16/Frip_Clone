1bda7fd54a7627e25651498182b9a425200ee406
KTH-INHA-16 <kim1120318@naver.com>
2021-01-13 19:36:28 +0900

    피드 버튼 눌렀을 때 디폴트 화면 추가

--- Frip/Frip/Feed/FeedUserViewController.swift
+++ Frip/Frip/Feed/FeedUserViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class FeedUserViewController: BaseViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "작성 가능한 후기"
+        label.font = UIFont.NotoSans(.bold, size: 20)
     }
 
     override func viewWillAppear(_ animated: Bool) {
